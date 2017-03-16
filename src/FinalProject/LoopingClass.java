a2cd36b92069f5d587d9f887ee0f7fac36c8a070
Vaughn01 <tv111790@students.davenport.k12.ia.us>
2017-03-16 14:00:32 -0500

    Merging Problems Again 3/16/2017

--- src/FinalProject/LoopingClass.java
+++ src/FinalProject/LoopingClass.java
@@ -12,8 +12,8 @@ import javax.swing.JOptionPane;
 public class LoopingClass 
 {
 
-	/**
-	 * @param args
+	/*This is part of the program where this will ask you if you want to know more about people. If the user selects yes then it will go
+	 * through the loop. If the user selects no then it will go straight through the story.
 	 */
 	public static void main(String[] args) 
 	{
